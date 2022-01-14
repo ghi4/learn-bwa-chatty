2a82e36d586d4bc21d983e5838953cc2971146d1
dhimas18 <ghidet4pcoop@gmail.com>
2022-01-14 13:59:20 +0700

    update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatty_app/theme.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,10 +7,41 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        "Hello guys",
-        style: TextStyle(fontSize: 40),
-      ),
-    );
+        backgroundColor: blueColor,
+        body: SafeArea(
+            child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              Image.asset(
+                "assets/images/profile_pic.png",
+                height: 100,
+                width: 100,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Sabrina Carpenter",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: whiteColor,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text(
+                "Travel Freelancer",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: lightBlueColor,
+                ),
+              ),
+            ],
+          ),
+        )));
   }
 }
---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+Color blueColor = Color(0xFF1F8DF5);
+Color whiteColor = Color(0xFFFFFFFF);
+Color lightBlueColor = Color(0xFF9ED0FF);
+Color blackColor = Color(0xFF9ED0FF);
+Color greyColor = Color(0xFF808BA2);
+Color greenColor = Color(0xFF29CB9E);
+
+TextStyle titleTextStyle =
+    TextStyle(color: blackColor, fontWeight: FontWeight.w500, fontSize: 16);
+
+TextStyle subtitleTextStyle =
+    TextStyle(color: greyColor, fontWeight: FontWeight.w300, fontSize: 14);
